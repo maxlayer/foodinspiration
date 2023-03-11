6864c4a622ca4a7cc152c220b79073a7ca70bfe4
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 14:59:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,66 +2,45 @@ import streamlit as st
 import pandas as pd
 import random
 
-# Define food table
-food_table = pd.DataFrame({
-    'food': ['Tortellini mit Käse Sahne Soße', 'Nutella Brot', 'Pizza bestellen', 'Vegetarisches Sushi', 'Ein Spiegelei', 'Tomate Mozarella Salat', 'Käsebrot', 'Sushi selber machen', 'Pizza selber machen', 'Nougat Bit Müsli', 'Porridge mit Peanutbutter', 'Cookies backen', 'Hefezopf mit Nutella', 'Obazda Brot'],
-    'salty': ['salty', 'sweet', 'salty', 'salty', 'salty', 'salty', 'salty', 'salty', 'salty', 'sweet', 'sweet', 'sweet', 'sweet', 'salty'],
-    'effort': [4, 1, 1, 1, 2, 2, 1, 9, 9, 2, 3, 6, 6, 4],
-    'takeaway': ['takeaway', 'cook', 'takeaway', 'takeaway', 'cook', 'cook', 'cook', 'cook', 'cook', 'cook', 'cook', 'cook', 'cook', 'cook']
-})
+st.set_page_config(page_title="Food Inspiration", page_icon=":fork_and_knife:", layout="wide")
 
-# Filter by sweet/salty
-def filter_salty_sweet(food_table, salty_sweet):
-    filtered_food_table = food_table[food_table['salty'] == salty_sweet]
-    return filtered_food_table
+# Load data from Excel file
+df = pd.read_excel("food_table.xlsx")
 
-# Filter by takeaway/cook at home
-def filter_takeaway_cook(food_table, takeaway_cook):
-    filtered_food_table = food_table[food_table['takeaway'] == takeaway_cook]
-    return filtered_food_table
+# Define filters
+herzhaft_options = ["All", "salty", "sweet"]
+effort_options = ["All", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+takeaway_options = ["All", "takeaway", "cook"]
 
-# Filter by effort
-def filter_effort(food_table, effort):
-    filtered_food_table = food_table[food_table['effort'] <= effort]
-    return filtered_food_table
+# Get user inputs for filters
+st.write("# Welcome to Food Inspiration!")
+st.write("Use the following filters to find your perfect food:")
 
-# Set up page
-st.title("Food Inspiration")
+herzhaft = st.selectbox("Salty or sweet?", herzhaft_options)
+if herzhaft == "salty":
+    df = df[df["herzhaft"] == "salty"]
+elif herzhaft == "sweet":
+    df = df[df["herzhaft"] == "sweet"]
 
-# Ask for salty or sweet
-salty_sweet = st.radio("Do you want something sweet or salty?", ('salty', 'sweet'))
+effort = st.selectbox("How much effort?", effort_options)
+if effort != "All":
+    df = df[df["effort"] == int(effort)]
 
-# Filter by salty/sweet
-filtered_food_table = filter_salty_sweet(food_table, salty_sweet)
-
-# Ask for takeaway or cook at home
-takeaway_cook = st.radio("Do you want to cook at home or get takeaway?", ('cook', 'takeaway'))
-
-# Filter by takeaway/cook at home
-filtered_food_table = filter_takeaway_cook(filtered_food_table, takeaway_cook)
-
-# Ask for effort level
-effort = st.slider("How much effort do you want to put in?", 1, 9)
-
-# Filter by effort
-filtered_food_table = filter_effort(filtered_food_table, effort)
+takeaway = st.selectbox("Takeaway or cook at home?", takeaway_options)
+if liefern != "All":
+    df = df[df["takeaway"] == takeaway]
 
 # Suggest food
-if st.button("Suggest Food"):
-    # Randomly select food from filtered options
-    food_options = list(filtered_food_table["food"])
-    if len(food_options) == 0:
-        st.write("Sorry, no food options match your preferences.")
+if st.button("Suggest food"):
+    if len(df) > 0:
+        current_food = random.choice(list(df["food"]))
+        st.write(f"Try {current_food}!")
     else:
-        current_food = random.choice(food_options)
-        st.write("You should try", current_food)
-        st.session_state.last_food = current_food
+        st.write("No food found with these filters.")
 
-# Suggest another food
-if st.button("Try Again"):
-    # Randomly select food from filtered options
-    food_options = list(filtered_food_table["food"])
-    if len(food_options) == 0:
-        st.write("Sorry, no food options match your preferences.")
-    else:
-        current_food = random.choice([x for x in food_options if
+# Try again button
+if st.button("Bäh! Ich will was leckres"):
+    st.experimental_rerun()
+
+st.write("---")
+st.write("Food data source: [www.example.com/food](www.example.com/food)")    
