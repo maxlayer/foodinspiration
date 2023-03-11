acd2d0e83984901ff80bda5c1ba3f89a5ebe7b56
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 22:52:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,13 +52,13 @@ document.addEventListener('DOMContentLoaded', function() {
 """
 st.set_page_config(page_title="Carlas Food Inspiration", page_icon=":fork_and_knife:", layout="wide")
 st.markdown("<div id='logo'><img src='https://www.freeiconspng.com/uploads/restaurant-icon-png-21.png' width='50'></div>", unsafe_allow_html=True)
-st.write("Hier muss man erst filtern:")
-st.markdown(JS, unsafe_allow_html=True)
 
 
 # Get user inputs for filters
 st.write("# Carlas Food Inspiration!")
 st.write("Hier muss man erst filtern:")
+st.markdown(JS, unsafe_allow_html=True)
+
 
 herzhaft = st.selectbox("herzhaft oder süß?", herzhaft_options)
 if herzhaft != "All":
@@ -77,7 +77,8 @@ if takeaway != "All":
 if st.button("WAS LECKRES"):
     if len(df) > 0:
         current_food = random.choice(list(df["food"]))
-        st.write(f"# {current_food}")
+        #st.write(f"# {current_food}")
+        st.write(f"<h1>{current_food}</h1>", unsafe_allow_html=True)
         st.write("---")
         st.button("Bäh! Ich will was leckres", key="new_suggestion")
     else:
