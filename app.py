8ca5760cd9f0703aed6735133ade44c8448eeeb8
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 23:43:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import random
 import time
+from streamlit_themes import set_theme
+
+set_theme('purple')
 
 # Load data from Excel file
 df = pd.read_excel("food_table.xlsx")
