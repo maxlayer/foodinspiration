008e2731522e6f716b9bc4b6516c3d6055d82a1e
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 20:28:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import random
-from st_aggrid import GridOptionsBuilder, AgGrid, DataReturnMode, JsCode
 
-st.set_page_config(page_title="Food Inspiration für Carla", page_icon=":fork_and_knife:", layout="wide")
+st.set_page_config(page_title="Carla Food Inspiration", page_icon=":fork_and_knife:", layout="wide")
 
 # Load data from Excel file
 df = pd.read_excel("food_table.xlsx")
@@ -11,11 +10,11 @@ df = pd.read_excel("food_table.xlsx")
 # Define filters
 herzhaft_options = ["All", "herzhaft", "süß"]
 takeaway_options = ["All", "bestellen", "kochen"]
-effort_options = ["All", "wenig", "mittel", "hoch", "bestellen"]
+effort_options = ["All", "wenig", "mittel", "hoch"]
 
 # Get user inputs for filters
-st.write("# Welcome to Food Inspiration!")
-st.write("Use the following filters to find your perfect food:")
+st.write("# Carlas Food Inspiration!")
+st.write("Hier muss man erst filtern:")
 
 herzhaft = st.selectbox("herzhaft oder süß?", herzhaft_options)
 if herzhaft != "All":
@@ -26,47 +25,24 @@ if takeaway != "All":
     df = df[df["takeaway"] == takeaway]
     
     if takeaway == "kochen":
-        effort = st.selectbox("How much effort?", effort_options)
+        effort = st.selectbox("Wie viel Aufwand?", effort_options)
         if effort != "All":
             df = df[df["effort"] == effort]
 
 # Suggest food
 if st.button("WAS LECKRES"):
     if len(df) > 0:
-        food_list = list(df["food"])
-        grid_options = GridOptionsBuilder.from_dataframe(df) \
-            .enable_selection() \
-            .enable_pagination() \
-            .enable_filtering() \
-            .enable_sorting() \
-            .pagination_autoPageSize(True) \
-            .build()
-
-        food_returned = AgGrid(
-            df,
-            gridOptions=grid_options,
-            data_return_mode=DataReturnMode.AS_ROWS,
-            update_mode=AgGrid.UpdateMode.SELECTION_CHANGED,
-            height=400,
-            width='100%',
-            theme='streamlit',
-            key="food_grid"
-        )
-
-        if food_returned['event'] == 'SelectionChanged':
-            selected_rows = food_returned['data']
-            liked_foods = []
-            disliked_foods = []
-            for row in selected_rows:
-                if row['liked'] == True:
-                    liked_foods.append(row['food'])
-                elif row['liked'] == False:
-                    disliked_foods.append(row['food'])
-            st.write("Liked Foods:")
-            st.write(liked_foods)
-            st.write("Disliked Foods:")
-            st.write(disliked_foods)
+        current_food = random.choice(list(df["food"]))
+        st.write(f"Was Leckres: {current_food}!")
+        st.write("---")
+        st.button("Bäh! Ich will was leckres", key="new_suggestion")
     else:
         st.write("Leider gibt es nichts Leckeres.")
 
-st.write("Food data source: [www.example.com/food](http://www.example.com/food)")
+# Generate new suggestion
+if "new_suggestion" in st.session_state:
+    current_food = random.choice(list(df["food"]))
+    st.write(f"Was Leckres: {current_food}!")
+    st.write("---")
+    st.button("Bäh! Ich will was leckres", key="new_suggestion")
+
