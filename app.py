738055b760df1da581da75c87794d1d37924675c
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 14:38:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,38 +2,36 @@ import streamlit as st
 import pandas as pd
 import random
 
-# Load data
-@st.cache
-def load_data(file_path):
-    return pd.read_excel(file_path)
+# Load data from Excel file
+food_table = pd.read_excel('food_table.xlsx')
 
-food_table = load_data('food_table.xlsx')
-
-# Filters
-herzhaft_filter = st.sidebar.radio("Salty or sweet?", ("Salty", "Sweet", "Any"))
-dauer_filter = st.sidebar.slider("Effort level (1-9)", 1, 9, (1, 9))
-liefern_filter = st.sidebar.checkbox("Only delivery/takeaway?")
+# Sidebar filters
+herzhaft = st.sidebar.selectbox("Salty or sweet?", ["Any", "Salty", "Sweet"])
+dauer = st.sidebar.slider("Effort", 1, 9, (1, 9), 1)
+liefern = st.sidebar.selectbox("Takeaway or cook at home?", ["Any", "Takeaway", "Cook at home"])
 
 # Apply filters
-filtered_food_table = food_table.copy()
-if herzhaft_filter != "Any":
-    filtered_food_table = filtered_food_table[filtered_food_table["herzhaft"] == herzhaft_filter.lower()]
-if dauer_filter != (1, 9):
-    filtered_food_table = filtered_food_table[filtered_food_table["dauer"].between(*dauer_filter)]
-if liefern_filter:
-    filtered_food_table = filtered_food_table[filtered_food_table["liefern"] == "ja"]
-
-# Shuffle
-if st.button("Reshuffle"):
-    current_food = random.choice(filtered_food_table["essen"])
+if herzhaft != "Any":
+    filtered_food_table = food_table.loc[food_table["herzhaft"] == herzhaft]
 else:
-    current_food = st.session_state.get("current_food")
-    if not current_food or current_food not in filtered_food_table["essen"].values:
+    filtered_food_table = food_table.copy()
+
+filtered_food_table = filtered_food_table.loc[(filtered_food_table["dauer"] >= dauer[0]) & (filtered_food_table["dauer"] <= dauer[1])]
+
+if liefern != "Any":
+    filtered_food_table = filtered_food_table.loc[filtered_food_table["liefern"] == liefern]
+
+# Display a random food
+if not filtered_food_table.empty:
+    current_food = random.choice(filtered_food_table["essen"])
+    st.write("How about", current_food, "?")
+else:
+    st.write("No food found with the selected filters.")
+
+# Reshuffle button
+if st.button("Reshuffle"):
+    if not filtered_food_table.empty:
         current_food = random.choice(filtered_food_table["essen"])
-    st.session_state.current_food = current_food
-
-# Display
-st.write("# What's for food today?")
-st.write(f"## {current_food}")
-
-
+        st.write("How about", current_food, "?")
+    else:
+        st.write("No food found with the selected filters.")
