b404ac1d7d4c41189fdc930950e132b6777cd98a
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 20:09:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,39 +8,38 @@ st.set_page_config(page_title="Food Inspiration für Carla", page_icon=":fork_an
 df = pd.read_excel("food_table.xlsx")
 
 # Define filters
-herzhaft_options = ["All", "salty", "sweet"]
-effort_options = ["All", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-takeaway_options = ["All", "takeaway", "cook"]
+herzhaft_options = ["All", "herzhaft", "süß"]
+takeaway_options = ["All", "bestellen", "kochen"]
+effort_options = ["All", "wenig", "mittel", "hoch", "bestellen"]
 
 # Get user inputs for filters
 st.write("# Welcome to Food Inspiration!")
 st.write("Use the following filters to find your perfect food:")
 
-herzhaft = st.selectbox("salty or sweet?", herzhaft_options)
-if herzhaft == "salty":
-    df = df[df["salty"] == "salty"]
-elif herzhaft == "sweet":
-    df = df[df["salty"] == "sweet"]
+herzhaft = st.selectbox("herzhaft oder süß?", herzhaft_options)
+if herzhaft != "All":
+    df = df[df["salty"] == herzhaft]
 
-takeaway = st.selectbox("Takeaway or cook at home?", takeaway_options)
+takeaway = st.selectbox("Kochen oder Bestellen?", takeaway_options)
 if takeaway != "All":
     df = df[df["takeaway"] == takeaway]
-
-effort = st.selectbox("How much effort?", effort_options)
-if effort != "All":
-    df = df[df["effort"] == int(effort)]
     
+    if takeaway == "kochen":
+        effort = st.selectbox("How much effort?", effort_options)
+        if effort != "All":
+            df = df[df["effort"] == effort]
+
 # Suggest food
 if st.button("WAS LECKRES"):
     if len(df) > 0:
         current_food = random.choice(list(df["food"]))
         st.write(f"Was Leckres: {current_food}!")
     else:
-        st.write("Es gibt leider nichts leckres.")
+        st.write("Leider gibt es nichts Leckeres.")
 
 # Try again button
 if st.button("Bäh! Ich will was leckres"):
     st.experimental_rerun()
 
 st.write("---")
-st.write("Food data source: [www.example.com/food](www.example.com/food)")    
+st.write("Food data source: [www.example.com/food](http://www.example.com/food)")
