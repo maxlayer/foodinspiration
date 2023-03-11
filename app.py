430f00877494d59f2b78df26b0a750ee833b663f
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 20:15:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import random
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import ClientSettings
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Food Inspiration für Carla", page_icon=":fork_and_knife:", layout="wide")
 
@@ -12,38 +15,84 @@ herzhaft_options = ["All", "herzhaft", "süß"]
 takeaway_options = ["All", "bestellen", "kochen"]
 effort_options = ["All", "wenig", "mittel", "hoch", "bestellen"]
 
-# Get user inputs for filters
-st.write("# Welcome to Food Inspiration!")
-st.write("Use the following filters to find your perfect food:")
+# Define variables for swipe cards
+swipe_cards = []
+swipe_index = 0
 
-herzhaft = st.selectbox("herzhaft oder süß?", herzhaft_options)
-if herzhaft != "All":
-    df = df[df["salty"] == herzhaft]
+# Define a class to capture video from the user's camera
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        return frame
 
-takeaway = st.selectbox("Kochen oder Bestellen?", takeaway_options)
-if takeaway != "All":
-    df = df[df["takeaway"] == takeaway]
-    
-    if takeaway == "kochen":
-        effort = st.selectbox("How much effort?", effort_options)
-        if effort != "All":
-            df = df[df["effort"] == effort]
+# Create a function to display a swipe card for a food suggestion
+def create_swipe_card(food):
+    return f"""
+        <div class="swipe-card">
+            <h2>{food}</h2>
+        </div>
+    """
 
-# Suggest food
-if st.button("WAS LECKRES"):
-    if len(df) > 0:
-        current_food = random.choice(list(df["food"]))
-        st.write(f"Was Leckres: {current_food}!")
-        st.write("---")
-        st.button("Bäh! Ich will was leckres", key="new_suggestion")
-    else:
-        st.write("Leider gibt es nichts Leckeres.")
+# Create a function to generate swipe cards for all food suggestions in the dataframe
+def generate_swipe_cards():
+    global swipe_cards
+    global swipe_index
+    swipe_cards = [create_swipe_card(food) for food in df["food"]]
+    swipe_index = 0
 
-# Generate new suggestion
-if "new_suggestion" in st.session_state:
-    current_food = random.choice(list(df["food"]))
-    st.write(f"Was Leckres: {current_food}!")
-    st.write("---")
-    st.button("Bäh! Ich will was leckres", key="new_suggestion")
+# Generate swipe cards when the app starts
+generate_swipe_cards()
 
-st.write("Food data source: [www.example.com/food](http://www.example.com/food)")
+# Define the HTML and CSS for the swipe cards and swipe container
+swipe_css = """
+    <style>
+    .swipe-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+    }
+
+    .swipe-card {
+        background-color: white;
+        border-radius: 10px;
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+        width: 80%;
+        height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        z-index: 1;
+        transform: translateX(-50%) translateY(-50%) rotate(0deg);
+        transition: transform 0.5s ease;
+    }
+
+    .swipe-card h2 {
+        font-size: 48px;
+        text-align: center;
+    }
+
+    .swipe-card.yes {
+        transform: translateX(-50%) translateY(-50%) rotate(30deg) translateX(500px);
+    }
+
+    .swipe-card.no {
+        transform: translateX(-50%) translateY(-50%) rotate(-30deg) translateX(-500px);
+    }
+    </style>
+"""
+
+swipe_html = f"""
+    <div class="swipe-container">
+        <div id="swipe-card-container">
+            {"".join(swipe_cards)}
+        </div>
+    </div>
+"""
+
+# Define the JavaScript for the swipe events
+swipe_js = """
+    <script src="https://hammerjs.github.io/dist/hammer.js"></script>
+    <script>
+    var swipeContainer = document.getElementById('swipe-card-container');
+    var cards = swipeContainer.getElementsByClassName('swipe-card
