714d31f38ea387b48ae683cfbd3b78751e815986
maxlayer <64026268+maxlayer@users.noreply.github.com>
2023-03-11 20:18:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import random
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-from streamlit_webrtc import ClientSettings
-import streamlit.components.v1 as components
+from streamlit_aggrid import GridOptionsBuilder, AgGrid, DataReturnMode, JsCode
 
 st.set_page_config(page_title="Food Inspiration für Carla", page_icon=":fork_and_knife:", layout="wide")
 
@@ -15,84 +13,60 @@ herzhaft_options = ["All", "herzhaft", "süß"]
 takeaway_options = ["All", "bestellen", "kochen"]
 effort_options = ["All", "wenig", "mittel", "hoch", "bestellen"]
 
-# Define variables for swipe cards
-swipe_cards = []
-swipe_index = 0
+# Get user inputs for filters
+st.write("# Welcome to Food Inspiration!")
+st.write("Use the following filters to find your perfect food:")
 
-# Define a class to capture video from the user's camera
-class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        return frame
+herzhaft = st.selectbox("herzhaft oder süß?", herzhaft_options)
+if herzhaft != "All":
+    df = df[df["salty"] == herzhaft]
 
-# Create a function to display a swipe card for a food suggestion
-def create_swipe_card(food):
-    return f"""
-        <div class="swipe-card">
-            <h2>{food}</h2>
-        </div>
-    """
+takeaway = st.selectbox("Kochen oder Bestellen?", takeaway_options)
+if takeaway != "All":
+    df = df[df["takeaway"] == takeaway]
+    
+    if takeaway == "kochen":
+        effort = st.selectbox("How much effort?", effort_options)
+        if effort != "All":
+            df = df[df["effort"] == effort]
 
-# Create a function to generate swipe cards for all food suggestions in the dataframe
-def generate_swipe_cards():
-    global swipe_cards
-    global swipe_index
-    swipe_cards = [create_swipe_card(food) for food in df["food"]]
-    swipe_index = 0
+# Suggest food
+if st.button("WAS LECKRES"):
+    if len(df) > 0:
+        food_list = list(df["food"])
+        grid_options = GridOptionsBuilder.from_dataframe(df) \
+            .enable_selection() \
+            .enable_pagination() \
+            .enable_filtering() \
+            .enable_sorting() \
+            .pagination_autoPageSize(True) \
+            .build()
 
-# Generate swipe cards when the app starts
-generate_swipe_cards()
+        food_returned = AgGrid(
+            df,
+            gridOptions=grid_options,
+            data_return_mode=DataReturnMode.AS_ROWS,
+            update_mode=AgGrid.UpdateMode.SELECTION_CHANGED,
+            height=400,
+            width='100%',
+            theme='streamlit',
+            key="food_grid"
+        )
 
-# Define the HTML and CSS for the swipe cards and swipe container
-swipe_css = """
-    <style>
-    .swipe-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px;
-    }
+        if food_returned['event'] == 'SelectionChanged':
+            selected_rows = food_returned['data']
+            liked_foods = []
+            disliked_foods = []
+            for row in selected_rows:
+                if row['liked'] == True:
+                    liked_foods.append(row['food'])
+                elif row['liked'] == False:
+                    disliked_foods.append(row['food'])
+            st.write("Liked Foods:")
+            st.write(liked_foods)
+            st.write("Disliked Foods:")
+            st.write(disliked_foods)
+    else:
+        st.write("Leider gibt es nichts Leckeres.")
 
-    .swipe-card {
-        background-color: white;
-        border-radius: 10px;
-        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-        width: 80%;
-        height: 300px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        z-index: 1;
-        transform: translateX(-50%) translateY(-50%) rotate(0deg);
-        transition: transform 0.5s ease;
-    }
-
-    .swipe-card h2 {
-        font-size: 48px;
-        text-align: center;
-    }
-
-    .swipe-card.yes {
-        transform: translateX(-50%) translateY(-50%) rotate(30deg) translateX(500px);
-    }
-
-    .swipe-card.no {
-        transform: translateX(-50%) translateY(-50%) rotate(-30deg) translateX(-500px);
-    }
-    </style>
-"""
-
-swipe_html = f"""
-    <div class="swipe-container">
-        <div id="swipe-card-container">
-            {"".join(swipe_cards)}
-        </div>
-    </div>
-"""
-
-# Define the JavaScript for the swipe events
-swipe_js = """
-    <script src="https://hammerjs.github.io/dist/hammer.js"></script>
-    <script>
-    var swipeContainer = document.getElementById('swipe-card-container');
-    var cards = swipeContainer.getElementsByClassName('swipe-card
+st.write("Food data source: [www.example.com/food](http://www.example.com/food)")
